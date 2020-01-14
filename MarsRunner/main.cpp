b04ae9efc7aa5bd954727904b5c4e23d69cb0fb8
LarsFL <lars.loijenga@gmail.com>
2020-01-14 14:06:24 +0100

    Init

--- MarsRunner/main.cpp
+++ MarsRunner/main.cpp
@@ -1,22 +1,25 @@
 #include <SFML/Graphics.hpp>
 
+#include "Code/Game engine/Object systems/GameObject.hpp"
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
+   
+    std::string thing = "../Assets/Test/testplaatje.png";
+    GameObject object{ thing, sf::Vector2f{0,0}, sf::Vector2f{0.1,0.1}, 5 };
 
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
         window.clear();
-        window.draw(shape);
+        object.draw(window);
         window.display();
     }
 
