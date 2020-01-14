505be7f7418947f12022bd141173f2ee6fc4aada
MartNotermans <43001231+MartNotermans@users.noreply.github.com>
2020-01-14 13:45:02 +0100

    input system done

---
+++ MarsRunner/Code/Game engine/Input systems/input.hpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <functional>
+#include <SFML/Graphics.hpp>
+
+class action {
+private:
+	std::function< bool() > condition;
+	std::function< void() > work;
+public:
+	action(
+		std::function< bool() > condition,
+		std::function< void() > work
+	) : condition(condition),
+		work(work)
+	{}
+
+	action(
+		sf::Keyboard::Key key,
+		std::function< void() > work
+	) :
+		condition(
+			[key]()->bool { return sf::Keyboard::isKeyPressed(key); }
+		),
+		work(work)
+	{}
+
+	action(
+		sf::Mouse::Button button,
+		std::function< void() > work
+	) :
+		condition(
+			[button]()->bool { return sf::Mouse::isButtonPressed(button); }
+		),
+		work(work)
+	{}
+
+	void operator()() {
+		if (condition()) {
+			work();
+		}
+	}
+};
